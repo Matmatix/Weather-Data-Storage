cdfc99080337e8b7c9f0147cf022d41058a98b93
Matmatix <mattkinz96@gmail.com>
2018-04-04 17:57:13 -0500

    Renamed overloaded variables

--- table.py
+++ table.py
@@ -1,12 +1,12 @@
 #This will be the class for the hash table
 
 class HashTable:
-    def __init__(self, length, hash):
+    def __init__(self, length, newHash):
         if isinstance(length) != int or length <= 0: #make the table at least one long
             length = 1
-        if isinstance(hash) != int or hash < 0 or hash > 1:           #this integer value(can possibly change later) will be used to choose a hashing alogrithm for this specific hash table
-            hash = 0
-        self.hash = hash
+        if isinstance(newHash) != int or newHash < 0 or newHash > 1:           #this integer value(can possibly change later) will be used to choose a hashing alogrithm for this specific hash table
+            newHash = 0
+        self.newHash = newHash
         self.length = length
         self.slots = [None for _ in range(0, self.length)]      #create a list of all of the slots for objects in the table, possibly other lists in each spot as we are chaining
 
@@ -14,19 +14,19 @@ class HashTable:
     def hashLocation(self, key):
         #use the hash number to specify which algorithm to use to hash the key
         #ie use the hasing class maybe
-        if self.hash == 0:                  #simplest version everything goes to the first slot
+        if self.newHash == 0:                  #simplest version everything goes to the first slot
             return 0
 
     #add a new object to the table
-    def add(self, key, object):
+    def add(self, key, obj):
         location = self.hashLocation(key)                       #get the location to hash to
         if self.slots[location] is None:                        #check if the slot is filled if not add the object
-            self.slots[location] = object
+            self.slots[location] = obj
         elif isinstance(self.slots[location]) != list:             #if the slot already has a singular object in it replace with a list
             temp = self.slots[location]
-            self.slots[location] = [temp, object]
+            self.slots[location] = [temp, obj]
         else:                                                   #if already a list append object to the end of the list
-            self.slots[location].append(object)
+            self.slots[location].append(obj)
 
     #update data that is already in the hash table
     def update(self, key, update):
